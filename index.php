12ff277c3e19b186e8b8cbce5de8afd4150ffbeb
DavidChaussinand <lebib07@live.fr>
2023-06-29 11:17:20 +0200

    tableau des routes.

--- index.php
+++ index.php
@@ -3,20 +3,23 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// déclaration du debut de la session start . on doit mettre dès l'ouverture de la balise php
-// vu que l'index.php est le fichier racine qui relie les autres pages, je déclare ici.
 
-$pagefilter = filter_input(INPUT_GET,'page',FILTER_SANITIZE_URL);
 
-if  ($pagefilter  == ''){
-require 'pages/accueil.php';
-} elseif ($pagefilter == 'contact'){
-require 'pages/contact.php';
-} elseif ($pagefilter == 'hobby') {
-require 'pages/hobby.php';
-} else  {
-echo "ErrorDocument 404 ";
+
+$pagefilter = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_URL);
+
+$routes = [
+    '' => 'pages/accueil.php',
+    'contact' => 'pages/contact.php',
+    'hobby' => 'pages/hobby.php'
+];
+
+
+if (isset($routes[$pagefilter])) {
+    require $routes[$pagefilter];
+} else {
+    echo "ErrorDocument 404";
 }
+
+
 ?>
-
-
