72a20acd273ea379ff7ac75762263b5fc9e58a6e
DavidChaussinand <lebib07@live.fr>
2023-06-30 13:35:47 +0200

    étapes 6-4 implémentez la fonction lastBlogPosts

--- app/persistences/blogPostData.php
+++ app/persistences/blogPostData.php
@@ -5,18 +5,11 @@ function lastBlogPosts(PDO $pdo)
     {   
 
         $articles = [];
-
-
-        // $last10Articles = 'SELECT * FROM articles ORDER BY id DESC LIMIT 10';
-        // $recipesStatement = $pdo->prepare($last10Articles);
-        // $recipesStatement->execute();
-
-        // $articles = $recipesStatement->fetchAll();
+        $sql = file_get_contents('database/lastBlogPosts.sql'); // récupération du contenu de mon fichier lastBlogPosts.sql
+        $last10Articles = $sql; 
+        $recipesStatement = $pdo->prepare($last10Articles);
+        $recipesStatement->execute();
+        $articles = $recipesStatement->fetchAll();
         
         return $articles;
-        
-
-    
 }
-
-?>
